c2d2e2540e88b087cbb991e3b965dd3e460d1dcb
Jong012 <Jong012>
2022-12-06 15:56:35 +0900

    feat(tts.views): Project, Audio viewset 생성

--- backend/tts/views.py
+++ backend/tts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from tts.models import Audio, Project
+from tts.serializers import AudioSerializer, ProjectSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def create(self, request, *args, **kwargs):
+        pass
+
+
+class AudioViewSet(viewsets.ModelViewSet):
+    queryset = Audio.objects.all()
+    serializer_class = AudioSerializer
